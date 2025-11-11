e4287f4e2850d027838afed972db18b931a8ae2a
Mateus Acauhi <acauhi.mateus@gmail.com>
2025-11-10 21:36:38 -0300

    implement HTTP server initialization and CORS middleware

--- main.go
+++ main.go
@@ -11,11 +11,32 @@ import (
 
 // main inicializa o servidor HTTP com todas as dependências
 func main() {
-	// TODO: implementar
+	repo := repository.NewInMemoryTaskRepository()
+	svc := service.NewTaskService(repo)
+	handler := handlers.NewTaskHandler(svc)
+
+	mux := http.NewServeMux()
+	mux.Handle("/tasks", corsMiddleware(handler))
+	mux.Handle("/tasks/", corsMiddleware(handler))
+
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // corsMiddleware adiciona headers CORS para permitir requisições do frontend
 func corsMiddleware(next http.Handler) http.Handler {
-	// TODO: implementar
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
